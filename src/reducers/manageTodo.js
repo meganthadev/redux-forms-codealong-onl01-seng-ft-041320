8f9e703884352340b6c6db995b8317b596bf64aa
Megan Horsfield <meganthadev@gmail.com>
2020-08-31 06:26:24 +0000

    Automatically backed up by Learn

--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -1,9 +1,4 @@
-export default function manageTodo(state = {
-  todos: [
-    { text: 'buy groceries' },
-    { text: 'watch netflix' },
-  ]
-}, action) {
+export default function manageTodo(state = { todos: [] }, action) {
 
   console.log("reducer received this action:", action);
 
